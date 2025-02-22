b9e6faa205c0aa380923899146413a0fe999f7fe
ASUS <rayshaayuda@gmail.com>
2025-02-22 12:57:51 +0700

    update

---
+++ lib/Screen/page_dua.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class PageDua extends StatelessWidget {
+  const PageDua({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Dua'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Center(
+        child: Row (
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Icon(
+              Icons.home,
+              size: 35,
+                color: Colors.green,
+
+            ),
+            Icon(
+              Icons.spa,
+              size: 35,
+              color: Colors.purple,
+            ),
+            Icon(
+              Icons.phone,
+              size: 35,
+              color: Colors.pink,
+            ),
+          ],
+        ),
+
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mi2b/Screen/page_dua.dart';
+import 'package:mi2b/Screen/page_satu.dart';
+import 'package:mi2b/Screen/page_tiga.dart';
+import 'package:mi2b/Screen/page_empat.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,27 +11,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -37,7 +25,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//ketik st
 class PageOne extends StatelessWidget {
   const PageOne({super.key});
 
@@ -45,18 +32,71 @@ class PageOne extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        //appBar : properti dari sebuah widget
-        //AppBar : widget
-
-        title: Text('Aplikasi Pertama',
+        title: Text(
+          'Aplikasi Pertama',
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.purple,
       ),
       body: Center(
-        child: Text("Selamat Datang di Flutter App pertama Raysha Ayunda"),
-        //child :  cuma bisa nampung 1 widget
-        //children : bisa nampung beberapa widget
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Selamat Datang di Flutter App pertama Mi2b Raysha"),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => PageSatu()));
+              },
+              color: Colors.purple,
+              child: Text(
+                'Page 1',
+                style: TextStyle(fontSize: 14, color: Colors.white),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => PageDua()));
+                },
+                elevation: 18.0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                color: Color(0x7958089E),
+                clipBehavior: Clip.antiAlias,
+                child: Text(
+                  'Page 2',
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => PageTiga()));
+              },
+              color: Colors.purple,
+              child: Text(
+                'Page 3',
+                style: TextStyle(fontSize: 14, color: Colors.white),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => PageEmpat()));
+              },
+              color: Colors.purple,
+              child: Text(
+                'Page 4',
+                style: TextStyle(fontSize: 14, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
